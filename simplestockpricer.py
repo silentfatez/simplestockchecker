8525c527d677210c6dc5c6dfa48e59296fc1aac0
silentfatez <41805687+silentfatez@users.noreply.github.com>
2020-07-29 20:10:40 +0800

    Update simplestockpricer.py

--- simplestockpricer.py
+++ simplestockpricer.py
@@ -10,21 +10,35 @@ growthestimates=float(analyst_info['Growth Estimates'][ticker][4].strip('%'))/10
 stats=get_stats(ticker)
 epsttm=float(stats['Value'][40])
 minrate=0.15
-marginsafety=0.33
+marginsafety=0.5
 current_market_price=get_live_price(ticker)
 
-url = 'http://financials.morningstar.com/valuate/current-valuation-list.action?&t=XNAS:'+ticker+'&region=usa&culture=en-US&cur=&adsFlag=true&_=1596015761530'
+url = 'http://financials.morningstar.com/valuate/current-valuation-list.action?&t=XNAS:'+ticker+'&region=usa&culture=en-US&cur=&adsFlag=true&_=1596023281217'
 source = urllib.request.urlopen(url).read()
 
 # Parse the html content
-
-soup = BeautifulSoup(source, "lxml")
-table = soup.find("table", attrs={"class": "r_table1 text2"})
-df = pd.read_html(str(table))[0]
-p_e=df[ticker+' 5Y Avg*'][1]
-if p_e=='—':
-    print('5y not found using double growth rate')
-    p_e=2*growthestimates
+try:
+    soup = BeautifulSoup(source, "lxml")
+    table = soup.find("table", attrs={"class": "r_table1 text2"})
+    df = pd.read_html(str(table))[0]
+    p_e=df[ticker+' 5Y Avg*'][1]
+    if p_e=='—':
+        print('5y not found using double growth rate')
+        p_e=2*growthestimates
+except:
+    try:##doesnt work for Visa need to use selenium maybe in the future
+        url='http://financials.morningstar.com/valuate/current-valuation-list.action?&t=XNYS:'+ticker+'&region=usa&culture=en-US&cur=&adsFlag=true&_=1596023281217'
+        source = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(source, "lxml")
+        table = soup.find("table", attrs={"class": "r_table1 text2"})
+        df = pd.read_html(str(table))[0]
+        p_e=df[ticker+' 5Y Avg*'][1]
+        if p_e=='—':
+            print('5y not found using double growth rate')
+            p_e=2*growthestimates
+    except:
+        print('5y not found using double growth rate')
+        p_e=2*growthestimates
 p_e=float(p_e)
 if p_e>2*growthestimates*100:
     p_e=2*growthestimates*100
@@ -32,6 +46,7 @@ futurevalue=epsttm
 for i in range(9):
     futurevalue+=futurevalue*growthestimates
 futureshareprice=futurevalue*p_e
+
 currentshareprice=futureshareprice
 for i in range(9):
     currentshareprice=currentshareprice/(1+minrate)
